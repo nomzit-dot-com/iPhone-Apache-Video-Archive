c910ed9a38a669b3d1673af85f9ff5ce98e5e2f1
Philip Graham Willoughby <willerz+github@gmail.com>
2010-12-08 19:44:12 +0000

    Improve video sizing
    Pick the largest size with the same aspect-ratio as the source video which will
    fit on the display. Designed for iPad but should also work nicely on other
    devices which play videos within the browser interface.

--- iPhoneVideoArchive.js
+++ iPhoneVideoArchive.js
@@ -21,17 +21,12 @@ $(document).ready(function(){
       {
         $('#video video').each(function()
         {
-          var height = this.videoHeight;
-          var width = this.videoWidth;
-          if (width > window.innerWidth)
-          {
-            height = height * window.innerWidth / width;
-            width = window.innerWidth;
-          }
+          var width = window.innerWidth;
+          var height = width * this.videoHeight / this.videoWidth;
           if (height > window.innerHeight)
           {
-            width = width * window.innerHeight / height;
             height = window.innerHeight;
+            width = height * this.videoWidth / this.videoHeight;
           }
           this.width = width;
           this.height = height;
